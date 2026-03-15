4a4e3ac94ebfbdcebff4a5dc5e31d5a36ce16bd5
universalcodes <yogeshkamra25@gmail.com>
2026-03-15 20:33:15 +0530

    Day 21 = Completed Contains with Text Validate Element Present as False and True is Done.

--- src/b/Selenium/chapter2/blocators/xpathsExamples/xpaths_functions/BXpathFunctionContainsElementCheck.java
+++ src/b/Selenium/chapter2/blocators/xpathsExamples/xpaths_functions/BXpathFunctionContainsElementCheck.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BXpathFunctionContains {
+public class BXpathFunctionContainsElementCheck {
     /*
     Points to Remember
     1 Xpath Functions helps to improve to write webelements better there are different types of xpath Functions
@@ -38,14 +38,9 @@ public class BXpathFunctionContains {
             WebElement findElementByContainsWithTextFunctionUsingAsterisk = openBrowser.findElement(By.xpath("//*[contains(text(), 'Note')]")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
             System.out.println(findElementByContainsWithTextFunctionUsingAsterisk.isDisplayed());   // Return true if Element is found in the webpage otherwise false
 
-            boolean checkElement = openBrowser.findElements(By.xpath("//*[contains(text(), 'NOTE')]")).size()>0;
-            System.out.println(checkElement);   // Return true if Element is found in the webpage otherwise false
-
-
             System.out.println("Find the Particular HTML Text with Uppercase using contains with text() Function * means Any Tag");
             WebElement findSameElementUppercaseByContainsWithTextFunctionUsingAsterisk = openBrowser.findElement(By.xpath("//*[contains(text(), 'NOTE')]")); // In the Following Code we are tried to find same html text given in Uppercase using text() function using  * means Any T
             System.out.println(findSameElementUppercaseByContainsWithTextFunctionUsingAsterisk.isDisplayed());   // Return true if Element is found in the webpage otherwise false
-
 
             System.out.println("Find the Particular HTML Text with lowercase contains with  text() Function * means Any Tag");
             WebElement findSameElementLowerCaseByContainsWithTextFunctionUsingAsterisk = openBrowser.findElement(By.xpath("//*[contains(text(), 'note')]")); // In the Following Code we are tried to find same html text given in lowercase using text() function using  * means Any T
@@ -55,21 +50,29 @@ public class BXpathFunctionContains {
             WebElement findSameElementToggleCaseByContainsWithTextFunctionUsingAsterisk = openBrowser.findElement(By.xpath("//*[contains(text(), 'NoTe')]")); // In the Following Code we are tried to find same html text given in togglecase using text() function using  * means Any T
             System.out.println(findSameElementToggleCaseByContainsWithTextFunctionUsingAsterisk.isDisplayed());   // Return true if Element is found in the webpage otherwise false
 
+            System.out.println("Find the Particular HTML Text Element with Extra Space using contains with text() Function * means Any Tag");
+            WebElement findElementWithExtraSpaceByContainsWithTextFunctionUsingAsterisk = openBrowser.findElement(By.xpath("//*[text()='Note ']")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
+            System.out.println(findElementWithExtraSpaceByContainsWithTextFunctionUsingAsterisk.isDisplayed());   // Return true if Element is found in the webpage otherwise false
+
             System.out.println("Find the Particular HTML Text using contains with text() Function using Particular HTML Tag");
             WebElement findElementByContainsWithTextFunctionUsingHTMLTag = openBrowser.findElement(By.xpath("//td[contains(text(), 'Notebook')]")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text.       System.out.println(findElementByTextFunctionUsingHTMLTag.isDisplayed());   // Return true if Element is found in the webpage otherwise false
             System.out.println(findElementByContainsWithTextFunctionUsingHTMLTag.isDisplayed());   // Return true if Element is found in the webpage otherwise false
 
             System.out.println("Find the Particular HTML Text with Uppercase using contains with text() Function using Particular HTML Tag");
-            WebElement findSameElementUppercaseByTextFunctionUsingHTMLTag = openBrowser.findElement(By.xpath("//*[contains(text(), 'NOTE')]")); // In the Following Code we are tried to find same html text given in Uppercase using text() function using  * means Any T
-            System.out.println(findSameElementUppercaseByTextFunctionUsingHTMLTag.isDisplayed());   // Return true if Element is found in the webpage otherwise false
+            WebElement findSameElementUpperCaseByContainsWithTextFunctionUsingHTMLTag = openBrowser.findElement(By.xpath("//td[contains(text(), 'NOTE')]")); // In the Following Code we are tried to find same html text given in Uppercase using text() function using  * means Any T
+            System.out.println(findSameElementUpperCaseByContainsWithTextFunctionUsingHTMLTag.isDisplayed());   // Return true if Element is found in the webpage otherwise false
 
-            System.out.println("Find the Particular HTML Text with lowercase contains with  text() Function * means Any Tag");
-            WebElement findSameElementLowerCaseByTextFunctionUsingHTMLTag = openBrowser.findElement(By.xpath("//*[contains(text(), 'note')]")); // In the Following Code we are tried to find same html text given in lowercase using text() function using  * means Any T
+            System.out.println("Find the Particular HTML Text with lowercase contains with  text() Function using Particular HTML Tag");
+            WebElement findSameElementLowerCaseByTextFunctionUsingHTMLTag = openBrowser.findElement(By.xpath("//td[contains(text(), 'note')]")); // In the Following Code we are tried to find same html text given in lowercase using text() function using  * means Any T
             System.out.println(findSameElementLowerCaseByTextFunctionUsingHTMLTag.isDisplayed());   // Return true if Element is found in the webpage otherwise false
 
-            System.out.println("Find the Particular HTML Text with toggle contains with using text() Function * means Any Tag");
-            WebElement findSameElementToggleByTextFunctionUsingAsterisk = openBrowser.findElement(By.xpath("//*[contains(text(), 'NoTe')]")); // In the Following Code we are tried to find same html text given in togglecase using text() function using  * means Any T
+            System.out.println("Find the Particular HTML Text with toggle contains with using text() Function using Particular HTML Tag");
+            WebElement findSameElementToggleByTextFunctionUsingAsterisk = openBrowser.findElement(By.xpath("//td[contains(text(), 'NoTe')]")); // In the Following Code we are tried to find same html text given in togglecase using text() function using  * means Any T
             System.out.println(findSameElementToggleByTextFunctionUsingAsterisk.isDisplayed());   // Return true if Element is found in the webpage otherwise false
+
+            System.out.println("Find the Particular HTML Text Element with Extra Space using contains with text() Function using Particular HTML Tag");
+            WebElement findElementWithExtraSpaceByContainsWithTextFunctionUsingHTMLTag = openBrowser.findElement(By.xpath("//td[text()='Note ']")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
+            System.out.println(findElementWithExtraSpaceByContainsWithTextFunctionUsingHTMLTag.isDisplayed());   // Return true if Element is found in the webpage otherwise false
 
         }
         catch(NoSuchElementException exceptionText)
