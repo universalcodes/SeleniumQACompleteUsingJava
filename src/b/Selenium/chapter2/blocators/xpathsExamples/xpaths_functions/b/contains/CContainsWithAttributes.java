042ca32fd1224c14b01a7799a96c82b9ddf6cc01
universalcodes <yogeshkamra25@gmail.com>
2026-04-03 19:11:19 +0530

    Day 22 = Started Find Elements through attributes in HTML .

--- src/b/Selenium/chapter2/blocators/xpathsExamples/xpaths_functions/b/contains/CContainsWithAttributes.java
+++ src/b/Selenium/chapter2/blocators/xpathsExamples/xpaths_functions/b/contains/CContainsWithAttributes.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CContainsWithAttributes {
     /*
     points to Remember
-    1. Contains is the type of xpath functions, which can apply to different attributes
+    1. Contain is the type of xpath functions, which can apply to different attributes
     such as id, name, and a lot more attributes.
 
     what is the HTML Attributes
@@ -31,17 +31,36 @@ public class CContainsWithAttributes {
         System.out.println("Launch Browser");
         WebDriver openBrowser = new ChromeDriver();
         openBrowser.get("file:///F:/Full%20Selenium%20Java%20Journey/CompleteQASelenium/HTMLCodes/seleniumpage.HTML");
-        System.out.println("Find the Attribute 1");
-        WebElement findElementThroughAttribute = openBrowser.findElement(By.xpath("//*[@title='Example iframe']")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
+        System.out.println("Find the Attribute using title");
+       // In the following Code we are tried to find the html element using attribute @title with valid exact value,
+        // * is known as wild card character which means any tag, we can use any tag to find the element with the help of attribute.
+        WebElement findElementThroughAttribute = openBrowser.findElement(By.xpath("//*[@title='Example iframe']"));
         System.out.println(findElementThroughAttribute.isDisplayed());   // Return true if Element is found in the webpage otherwise false
-        WebElement findElementThroughAttribute2 = openBrowser.findElement(By.xpath("//*[@datetime='2025']")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
+        System.out.println("Find the Attribute using datetime");
+        // In the following Code we are tried to find the html element using attribute @datetime with valid exact value,
+        // * is known as wild card character which means any tag, we can use any tag to find the element with the help of attribute.
+        WebElement findElementThroughAttribute2 = openBrowser.findElement(By.xpath("//*[@datetime='2025']"));
         System.out.println(findElementThroughAttribute2.isDisplayed());   // Return true if Element is found in the webpage otherwise false
-        WebElement findElementThroughAttribute3 = openBrowser.findElement(By.xpath("//*[@method='dialog']")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
+        System.out.println("Find the Attribute using method");
+        // In the following Code we are tried to find the html element using attribute @dialog with valid exact value,
+        // * is known as wild card character which means any tag, we can use any tag to find the element with the help of attribute.
+        WebElement findElementThroughAttribute3 = openBrowser.findElement(By.xpath("//*[@method='dialog']"));
         System.out.println(findElementThroughAttribute3.isDisplayed());   // Return true if Element is found in the webpage otherwise false
-        WebElement findElementThroughAttribute4 = openBrowser.findElement(By.xpath("//*[@id='interactive']")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
+        System.out.println("Find the Attribute using id");
+        // In the following Code we are tried to find the html element using attribute @id with valid exact value,
+        // * is known as wild card character which means any tag, we can use any tag to find the element with the help of attribute.
+        WebElement findElementThroughAttribute4 = openBrowser.findElement(By.xpath("//*[@id='interactive']"));
         System.out.println(findElementThroughAttribute4.isDisplayed());   // Return true if Element is found in the webpage otherwise false
-        WebElement findMultpleElementThroughAttribute = openBrowser.findElement(By.xpath("//*[@title='Example iframe'][@datetime='2025']")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
-        System.out.println(findMultpleElementThroughAttribute.isDisplayed());   // Return true if Element is found in the webpage otherwise false
+        System.out.println("Find the Attribute using multiple Attribute using title and datetime");
+        // In the following Code we are tried to find the html element using attribute @title & @height with valid exact value,
+        // * is known as wild card character which means any tag, we can use any tag to find the element with the help of attribute.
+        // in the case of multiple attributes one or more attributes should in the same tag, otherwise it will return false
+        // it thorw an exception as NoSuchElementException if any of the attribute is not found in the webpage
+        WebElement findMultipleElementThroughAttribute = openBrowser.findElement(By.xpath("//*[@title='Example iframe'][@height=200]")); // In the Following Code we are tried to find html element using contains with text(), we have provided partial text instead of complete text we have provided partial text. * means Any Tag
+        System.out.println(findMultipleElementThroughAttribute.isDisplayed());   // Return true if Element is found in the webpage otherwise false
+        System.out.println("Find the Attribute using title using contains()");
+        WebElement findElementThroughAttributeWithContains = openBrowser.findElement(By.xpath("//*[contains(@title, 'Example')]")); // In the Following Code we are tried to fin
+        System.out.println(findElementThroughAttributeWithContains.isDisplayed());   // Return true if Element is found in the webpage otherwise false
 
 
 
