71dfed0920c7aaca1a38e16b3710fd34af3e0a7e
universalcodes <yogeshkamra25@gmail.com>
2026-04-15 12:18:22 +0530

    Day 24 = Updated Normalize and Position

--- src/b/Selenium/locator/chapter2/b/advancedlocator/xpath/b/xpath/functions/EPositionAndLastXpathFunction.java
+++ src/b/Selenium/locator/chapter2/b/advancedlocator/xpath/b/xpath/functions/EPositionAndLastXpathFunction.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class EPositionXpathFunction {
+public class EPositionAndLastXpathFunction {
     /*
     Points to Remember
     1. Position() is an XPath function that returns the position of a node in a node set.
@@ -34,19 +34,12 @@ public class EPositionXpathFunction {
 
      */
 
-
-
-
     public static void main(String[] args) {
         System.out.println("Launch Browser");
         WebDriver openBrowser = new ChromeDriver();
         openBrowser.get("file:///F:/Full%20Selenium%20Java%20Journey/CompleteQASelenium/HTMLCodes/seleniumpage.HTML");
-//        System.out.println("find the invalid element with id 'input' using position() function");
-//        // As following xpath expression is incorrect, it throws runtime exception as InvalidSelectorException
-//        WebElement invalidElement = openBrowser.findElement(By.xpath("//*[@name='myyNameisUniqueAndTested'][position()=1)]"));
-//        System.out.println("The first element with id 'input' is: " + invalidElement.isDisplayed());
 
-        System.out.println("1.find the 1st element with id 'input' using position() function");
+     System.out.println("1.find the 1st element with id 'input' using position() function");
         // As following xpath expression is incorrect, it throws runtime exception as InvalidSelectorException
         WebElement firstElement = openBrowser.findElement(By.xpath("(//*[@name='myyNameisUniqueAndTested'])[position()=1]"));
         System.out.println("The first element with id 'input' is: " + firstElement.isDisplayed());
@@ -67,7 +60,13 @@ public class EPositionXpathFunction {
         System.out.println("find the last element with id 'input' using position() function");
         WebElement fifthElement = openBrowser.findElement(By.xpath("(//*[@name='myyNameisUniqueAndTested'])[last()]"));
         System.out.println("The first element with id 'input' is: " + fifthElement.isDisplayed());
-//
+
+
+        System.out.println("find the invalid element with id 'input' using position() function");
+//        // As following xpath expression is incorrect, it throws runtime exception as InvalidSelectorException
+//        WebElement invalidElement = openBrowser.findElement(By.xpath("//*[@name='myyNameisUniqueAndTested'][position()=1)]"));
+//        System.out.println("The first element with id 'input' is: " + invalidElement.isDisplayed());
+
 //                System.out.println("find the invalid element with id 'input' using position() function");
 //        // As following xpath expression is incorrect, it throws runtime exception as InvalidSelectorException
 //        WebElement invalidElement = openBrowser.findElement(By.xpath("//*[@name='myyNameisUniqueAndTested'][position()=one)]"));
