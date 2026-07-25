16b3d22dda8289c3506669a2d2a22561a042e1a9
universalcodes <yogeshkamra25@gmail.com>
2026-07-25 11:29:14 +0530

    Day 36 = Windows & Tabs Handling is Completed

--- src/a/Seleniumm/chapter1/basiccs/a1/windowHanding/ATabWindowHandling.java
+++ src/a/Seleniumm/chapter1/basiccs/a1/windowHanding/ATabWindowHandling.java
@@ -29,6 +29,12 @@ public class ATabWindowHandling {
        - getTitle() → Returns the title of the current tab/window.
        - close() → Closes the current tab/window.
        - quit() → Closes all tabs/windows and ends the browser session.
+
+       Some Notes
+        * Always call driver.switchTo().window(handle) before fetching title/URL.
+ * switchTo().newWindow(WindowType.TAB/WINDOW) → Selenium 4 feature.
+ * executeScript("window.open(...)") → Selenium 3 way to open new tab/window.
+ * Close child tabs/windows after operations, then return to parent.
 */
 
 
@@ -42,13 +48,13 @@ public class ATabWindowHandling {
         // The Following line 34  help us to maximize the Browser.
         openGoogleChrome.get("F:\\Full Selenium Java Journey\\CompleteQASelenium\\htmlCodes\\GetWindowHandles.html");
 
-         String parentMainWindow =    openGoogleChrome.getWindowHandle();
+        String parentMainWindow = openGoogleChrome.getWindowHandle();
         //Open a New Tab Through Selenium 4 ;
         openGoogleChrome.switchTo().newWindow(WindowType.TAB);
         openGoogleChrome.get("https://www.amazon.in/");
         String getTitleOfNewTab = openGoogleChrome.getTitle();
         String getCurrentOpenedWebAddress = openGoogleChrome.getCurrentUrl();
-        System.out.println("Title as follows:\t" +getTitleOfNewTab + " " + "Current WebAddress as follows:\t" +getCurrentOpenedWebAddress);
+        System.out.println("Title as follows:\t" + getTitleOfNewTab + " " + "Current WebAddress as follows:\t" + getCurrentOpenedWebAddress);
         // close the Current Newly Open Tab;
         Thread.sleep(10000);
         openGoogleChrome.close(); // close current Tab
@@ -61,7 +67,7 @@ public class ATabWindowHandling {
         openGoogleChrome.get("https://www.flipkart.com/");
         String getTitleOfNewWindow = openGoogleChrome.getTitle();
         String getCurrentOpenedWebAddressWindow = openGoogleChrome.getCurrentUrl();
-        System.out.println("Title as follows:\t" +getTitleOfNewWindow + " " + "Current WebAddress as follows:\t" +getCurrentOpenedWebAddressWindow);
+        System.out.println("Title as follows:\t" + getTitleOfNewWindow + " " + "Current WebAddress as follows:\t" + getCurrentOpenedWebAddressWindow);
         Thread.sleep(10000);
         openGoogleChrome.close(); // close the current Window
 
@@ -72,81 +78,91 @@ public class ATabWindowHandling {
         Thread.sleep(10000);
         String getCurrentOpenedTabTitleJS = openGoogleChrome.getTitle();
         String getCurrentOpenedTabAddressJS = openGoogleChrome.getCurrentUrl();
-        System.out.println("Title as follows:\t" +getCurrentOpenedTabTitleJS + " " + "Current WebAddress as follows:\t" +getCurrentOpenedTabAddressJS);
+        System.out.println("Title as follows:\t" + getCurrentOpenedTabTitleJS + " " + "Current WebAddress as follows:\t" + getCurrentOpenedTabAddressJS);
         //openGoogleChrome.close();
 
 
         // ✅ Open new Window using JS In Selenium 3
         //openGoogleChrome.switchTo().window(parentMainWindow);
+
+        JavascriptExecutor openNewWindow = ((JavascriptExecutor) openGoogleChrome);
+        openNewWindow.executeScript("window.open('https://blinkit.com/','blinkit','width=800,height=600');");
+                /*
+                            OR
         ((JavascriptExecutor) openGoogleChrome).executeScript("window.open('https://blinkit.com/','blinkit','width=800,height=600');");
+*/
+
+        String getMainTabHandleID = openGoogleChrome.getWindowHandle();
+        System.out.println("Webpage Tab Handle ID as follows\t" + getMainTabHandleID); // get the String Tab of the Webpage, Return type is String
+
+        WebElement openLinkA = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab']"));
+        openLinkA.click();
+        String getChildTabHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
+        System.out.println("Webpage Tab Handle ID as follows\t" + getChildTabHandleID);
+
+        WebElement openLinkB = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab 2']"));
+        openLinkB.click();
+        String getSecondChildTabHandleID = openGoogleChrome.getWindowHandle(); // Without Using Set It returns Same Tab ID
+        System.out.println("Webpage Tab Handle ID as follows\t" + getSecondChildTabHandleID);
+
+        WebElement openLinkC = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab 3']"));
+        openLinkC.click();
+        String getThirdChildTabHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
+        System.out.println("Webpage Tab Handle ID as follows\t" + getThirdChildTabHandleID);
+
+        WebElement openLinkD = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab 4']"));
+        openLinkD.click();
+        String getFourthChildTabHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
+        System.out.println("Webpage Tab Handle ID as follows\t" + getFourthChildTabHandleID);
+
+        WebElement openWindowA = openGoogleChrome.findElement(By.xpath("//*[text()='Open New Window']"));
+        openWindowA.click();
+        String getChildWindowHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
+        System.out.println("Webpage Tab Handle ID as follows\t" + getChildWindowHandleID);
 
 
+        WebElement openWindowB = openGoogleChrome.findElement(By.xpath("//*[text()='Another Window']"));
+        openWindowB.click();
+        String getChildWindowHandleIDB = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
+        System.out.println("Webpage Tab Handle ID as follows\t" + getChildWindowHandleIDB);
+
+        /*
+        Without Using Set It returns Same Tab ID
+        Set is Used To Iterate Each Tab & New Browser Window
+         */
+
+        Set<String> allTabWindowHandles = openGoogleChrome.getWindowHandles();
+        System.out.println("All Tab & Window Handles: " + allTabWindowHandles);
+
+
+        System.out.println("handle Browser Window And Tab Using for While loop");
+        for (String handleBrowsersTabWindows : openGoogleChrome.getWindowHandles()) {
+            System.out.println("Tab Handle Found:\t" + handleBrowsersTabWindows);
+        }
+
+        Iterator<String> TabHandleIterate = allTabWindowHandles.iterator();
+        /*
+        Using While Loop we are Iterating Each Tab using hasNext() & next() Method
+         */
+        while (TabHandleIterate.hasNext()) {
+            String handleDifferentWindowsTabs = TabHandleIterate.next();
+            if (!handleDifferentWindowsTabs.equals(getMainTabHandleID)) {
+                openGoogleChrome.switchTo().window(handleDifferentWindowsTabs);
+
+                String title = openGoogleChrome.getTitle();
+                String url = openGoogleChrome.getCurrentUrl();
+
+                System.out.println("Handle ID: " + handleDifferentWindowsTabs
+                        + " | Title: " + title
+                        + " | URL: " + url);
+
+                openGoogleChrome.close();
+            }
+        }
+    }
+}
 
 //
-//
-//        String getMainTabHandleID = openGoogleChrome.getWindowHandle();
-//        System.out.println("Webpage Tab Handle ID as follows\t" + getMainTabHandleID); // get the String Tab of the Webpage, Return type is String
-//
-//        WebElement openLinkA = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab']"));
-//        openLinkA.click();
-//        String getChildTabHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
-//        System.out.println("Webpage Tab Handle ID as follows\t" + getChildTabHandleID);
-//
-//        WebElement openLinkB = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab 2']"));
-//        openLinkB.click();
-//        String getSecondChildTabHandleID = openGoogleChrome.getWindowHandle(); // Without Using Set It returns Same Tab ID
-//        System.out.println("Webpage Tab Handle ID as follows\t" + getSecondChildTabHandleID);
-//
-//        WebElement openLinkC = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab 3']"));
-//        openLinkC.click();
-//        String getThirdChildTabHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
-//        System.out.println("Webpage Tab Handle ID as follows\t" + getThirdChildTabHandleID);
-//
-//        WebElement openLinkD = openGoogleChrome.findElement(By.xpath("//*[text()='Open Child Tab 4']"));
-//        openLinkD.click();
-//        String getFourthChildTabHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
-//        System.out.println("Webpage Tab Handle ID as follows\t" + getFourthChildTabHandleID);
-//
-//        WebElement openWindowA = openGoogleChrome.findElement(By.xpath("//*[text()='Open New Window']"));
-//        openWindowA.click();
-//        String getChildWindowHandleID = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
-//        System.out.println("Webpage Tab Handle ID as follows\t" + getChildWindowHandleID);
-//
-//
-//        WebElement openWindowB = openGoogleChrome.findElement(By.xpath("//*[text()='Another Window']"));
-//        openWindowB.click();
-//        String getChildWindowHandleIDB = openGoogleChrome.getWindowHandle();  // Without Using Set It returns Same Tab ID
-//        System.out.println("Webpage Tab Handle ID as follows\t" + getChildWindowHandleIDB);
-//
-//        /*
-//        Without Using Set It returns Same Tab ID
-//        Set is Used To Iterate Each Tab & New Browser Window
-//         */
-//
-//        Set<String> allTabWindowHandles = openGoogleChrome.getWindowHandles();
-//        System.out.println("All Tab & Window Handles: " + allTabWindowHandles);
-//
-//
-//        System.out.println("handle Browser Window And Tab Using for While loop");
-//        for (String handleBrowsersTabWindows : openGoogleChrome.getWindowHandles()) {
-//            System.out.println("Tab Handle Found:\t" + handleBrowsersTabWindows);
-//        }
-//
-//        Iterator<String> TabHandleIterate = allTabWindowHandles.iterator();
-//        /*
-//        Using While Loop we are Iterating Each Tab using hasNext() & next() Method
-//         */
-//        while (TabHandleIterate.hasNext()) {
-//            String handleDifferentWindowsTabs = TabHandleIterate.next();
-//            String getDifferentTabsWindowsTitle = openGoogleChrome.getTitle();
-//            if (!handleDifferentWindowsTabs.equals(getMainTabHandleID)) {
-//                openGoogleChrome.switchTo().window(handleDifferentWindowsTabs);
-//
-//                System.out.println("Window-Tab Handles ID as follows\t" + handleDifferentWindowsTabs + "  " + "Tab/Window URL Title as follows: " + getDifferentTabsWindowsTitle);
-//                 openGoogleChrome.close();
-//            }
-//
-////
 //        while(TabHandleIterate.hasNext()) {
 //            if(!(handleDifferentWindowsTabs.equals(getMainTabHandleID))) {
 //                String firstTabOrWindow = TabHandleIterate.next();
@@ -164,6 +180,3 @@ public class ATabWindowHandling {
 //            System.out.println("Third Tab Title: " + openGoogleChrome.getTitle());
 //        }
 //
-//        }
-    }
-}
